8e0e7bf12b1fdbc07c7245d54b5b3967bde95b86
karolis879 <karolis.vaiciulis123@gmail.com>
2025-05-27 14:48:40 +0300

    removed not needed files

--- routes/console.php
+++ routes/console.php
@@ -2,7 +2,14 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+Schedule::command('linkedin:fetch')->everyDay('12.00')->timezone('Europe/Vilnius')->onSuccess(function () {;
+    $this->comment('LinkedIn posts fetched successfully.');
+})->onFailure(function () {
+    $this->error('Failed to fetch LinkedIn posts.');
+});
